b025e66f93c50f3694d9b52c16ebeb957d9eade5
jyk1029 <jyk102999@dsm.hs.kr>
2022-08-06 02:32:15 +0900

    🔄 :: Config 패키지 경로 수정

--- src/main/java/com/example/entry/global/security/config/SecurityConfig.java
+++ src/main/java/com/example/entry/global/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.entry.global.config;
+package com.example.entry.global.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/example/entry/global/security/config/WebMvcConfig.java
+++ src/main/java/com/example/entry/global/security/config/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package com.example.entry.global.config;
+package com.example.entry.global.security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
