5dfaa2eb5f433360c5ce7e160c9b4b8605840d7f
jyk1029 <jyk102999@dsm.hs.kr>
2022-08-07 01:21:18 +0900

    ✅ :: User, Feed Entity 추가

---
+++ src/main/java/com/example/entry/domain/feed/domain/Feed.java
@@ -0,0 +1,36 @@
+package com.example.entry.domain.feed.domain;
+
+import com.example.entry.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Feed {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 20, nullable = false)
+    private String title;
+
+    @Column(length = 1000, nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public Feed(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+}
---
+++ src/main/java/com/example/entry/domain/user/domain/User.java
@@ -0,0 +1,38 @@
+package com.example.entry.domain.user.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 20, nullable = false)
+    private String accountId;
+
+    @Column(length = 60, nullable = false)
+    private String password;
+
+    @Column(length = 50, nullable = false)
+    private String name;
+
+    @Column(length = 4, nullable = false)
+    private int studentId;
+
+    @Builder
+    private User(String accountId, String password, String name, int studentId) {
+        this.accountId = accountId;
+        this.password = password;
+        this.name = name;
+        this.studentId = studentId;
+    }
+}
