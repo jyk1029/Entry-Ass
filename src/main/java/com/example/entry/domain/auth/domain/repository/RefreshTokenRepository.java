a5fed40ea77ef53b988006db784553eaace47a20
jyk1029 <jyk102999@dsm.hs.kr>
2022-08-13 04:22:03 +0900

    ✒ :: Repository 어노테이션 추가

--- src/main/java/com/example/entry/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/example/entry/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -2,9 +2,12 @@ package com.example.entry.domain.auth.domain.repository;
 
 import com.example.entry.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+
+@Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByRefreshToken(String refreshToken);
